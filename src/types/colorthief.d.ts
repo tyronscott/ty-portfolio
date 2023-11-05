5fece3d1f3dbe12f67845c5a033481114096c229
tyronscott <tyron_scott_lucena@dlsl.edu.ph>
2023-11-05 21:45:32 +0800

    Auto-generated commit

--- src/types/colorthief.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'colorthief' {
-	export type RGBColor = [number, number, number];
-	export default class ColorThief {
-		getColor: (img: HTMLImageElement | null, quality: number) => RGBColor;
-		getPalette: (img: HTMLImageElement | null, colorCount: number, quality: number) => RGBColor[];
-	}
-}
